07d53df4344cb95718dad833b5fda3f35c5b97ac
yusuf kukus <yusufkukus@gmail.com>
2021-10-25 23:11:27 +0300

    lesson 1

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("ındex sayfası");
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on  port ${port}`);
+});
